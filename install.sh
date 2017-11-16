81003806438ea9ab0e9413a97b084bd2f6570289
Mohammad Ashikuzzaman <ashikuzzaman.ar@gmail.com>
2017-11-16 14:34:50 +0600

    Bug-Fix : Fixing bug for removing user home directory

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-clear
+#clear
 
 # Initializing 
 USER_HOME="/home/$USER"
@@ -9,98 +9,113 @@ OJUL_HOME="$USER_HOME/.ojul"
 TEMP_HOME="$OJUL_HOME/temp"
 JAVA_HOME="$OJUL_HOME/oracle-java"
 
-#Checking for required directories
-echo "Checking directories ..."
-if [ ! -e $OJUL_HOME ]; then 
-    mkdir $OJUL_HOME
-    echo "$OJUL_HOME has been created ..."
+if [ ! -e $OJUL_HOME/release ]; then
+	
+	#Checking for required directories
+	echo "Checking directories ..."
+	if [ ! -e $OJUL_HOME ]; then 
+		mkdir $OJUL_HOME
+		echo "$OJUL_HOME has been created ..."
+	fi
+	if [ ! -e $TEMP_HOME ]; then
+		mkdir $TEMP_HOME
+		echo "$TEMP_HOME has been created ..."
+	fi
+	if [ ! -e $JAVA_HOME ]; then
+		mkdir $JAVA_HOME
+		echo "$JAVA_HOME has been created ..."
+	fi
+	echo "Directories are created ..."
+
+	# Checking and/or creatating source (.hashrc) file
+	if [ ! -e $BASHRC_FILE ]; then
+		touch $BASHRC_FILE
+	fi
+
+	# Exporting path variable to source (.hashrc) file
+	echo "" >> $BASHRC_FILE
+	echo "# Oracle JAVA Paths" >> $BASHRC_FILE
+	echo "export JAVA_HOME=$JAVA_HOME/java" >> $BASHRC_FILE
+	echo "export PATH=\$JAVA_HOME/bin:\$PATH" >> $BASHRC_FILE
+	echo "alias ojul='bash $OJUL_HOME/update.sh'" >> $BASHRC_FILE
+
+	#Reloading source (.bashrc) file
+	#source ~/.bashrc
+
+	# Copying all required/binary file for ojul to the application root directory
+	for FILE in `ls`
+	do
+		cp -rf $FILE $OJUL_HOME
+		echo "Copying $FILE to $OJUL_HOME ..."
+	done
+else
+	
+	# Message
+	echo "OJUL(Oracle JAVA Updater for Linux) is already installed ..."
 fi
-if [ ! -e $TEMP_HOME ]; then
-    mkdir $TEMP_HOME
-    echo "$TEMP_HOME has been created ..."
+
+if [ ! -e $JAVA_HOME/java/release ]; then
+
+	# Changing directory to ojul's temporary directory
+	cd $TEMP_HOME
+	echo "Directory changed to $TEMP_HOME ..."
+	echo "Cleaning $TEMP_HOME ..."
+
+	# Deleting all junk files from ojul's temporary directory
+	for FILE in `ls`
+	do
+		rm -rf $FILE
+		echo "Removed $FILE ..."
+	done
+
+	echo "Download is starting for Oracle JAVA from Oracle's official download server ...'"
+	echo "Please keep patience while downloading the archive ..."
+	echo "It may take some time according to your internet speed ..."
+
+	# Downloding Oracle JDK from Oracles official site
+	wget -c --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/9.0.1+11/jdk-9.0.1_linux-x64_bin.tar.gz"
+	echo "Download has been finished ..."
+
+	# Extracting and after that deleting downloaded archive file
+	for JAVA_ARCHIVE in `ls`
+	do
+		if [ -f $JAVA_ARCHIVE ]; then 
+			echo "Extrancting of $JAVA_ARCHIVE has been started..."
+			tar -xvf $JAVA_ARCHIVE -C $TEMP_HOME
+			echo "Extrancting has been finised..."
+			echo "Completely removing $JAVA_ARCHIVE ..."
+			rm -f $JAVA_ARCHIVE
+			echo "$JAVA_ARCHIVE has been removed completely..."
+		fi
+	done
+
+	# Renaming extracted directory name to 'java'
+	for JAVA_EXTRACTED_DIRECTORY in `ls`
+	do
+		if [ -d $JAVA_EXTRACTED_DIRECTORY ]; then 
+			echo "Renaming $JAVA_EXTRACTED_DIRECTORY to java ..."
+			mv -f $JAVA_EXTRACTED_DIRECTORY java
+			echo "Renamed $JAVA_EXTRACTED_DIRECTORY to java ..."
+		fi
+	done
+	echo "Moving java to $JAVA_HOME ..."
+
+	# Copying 'java' directory to 'java-home'
+	cp -rf java $JAVA_HOME
+	echo "java has been moden to $JAVA_HOME ..."
+	echo "Cleaning $TEMP_HOME ..."
+
+	# Cleaning ojul's temporary directory
+	#cd $TEMP_HOME
+	for JUNK_FILE in `ls`
+	do
+		rm -rf $JUNK_FILE
+	done
+	echo "Cleaning $TEMP_HOME has been finished..."
+
+	#Reloading source (.bashrc) file
+	source ~/.bashrc
 fi
-if [ ! -e $JAVA_HOME ]; then
-    mkdir $JAVA_HOME
-    echo "$JAVA_HOME has been created ..."
-fi
-echo "Directories are created ..."
-
-# Checking and/or creatating source (.hashrc) file
-if [ ! -e $BASHRC_FILE ]; then
-    touch $BASHRC_FILE
-fi
-
-# Exporting path variable to source (.hashrc) file
-echo "" >> $BASHRC_FILE
-echo "export JAVA_HOME=$JAVA_HOME/java" >> $BASHRC_FILE
-echo "export PATH=\$JAVA_HOME/bin:\$PATH" >> $BASHRC_FILE
-echo "alias ojul='bash $OJUL_HOME/update.sh'" >> $BASHRC_FILE
-
-#Reloading source (.bashrc) file
-source ~/.bashrc
-
-# Copying all required/binary file for ojul to the application root directory
-for FILE in `ls`
-do
-    cp -rf $FILE $OJUL_HOME
-    echo "Copying $FILE to $OJUL_HOME ..."
-done
-
-# Changing directory to ojul's temporary directory
-cd $TEMP_HOME
-echo "Directory changed to $TEMP_HOME ..."
-echo "Cleaning $TEMP_HOME ..."
-
-# Deleting all junk files from ojul's temporary directory
-for FILE in `ls`
-do
-    rm -rf $FILE
-    echo "Removed $FILE ..."
-done
-echo "Download is starting for Oracle JAVA from Oracle's official download server ...'"
-echo "Please be patience while downloading the archive ..."
-echo "It may take some time according to your internet speed ..."
-
-# Downloding Oracle JDK from Oracles official site
-wget -c --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/9.0.1+11/jdk-9.0.1_linux-x64_bin.tar.gz"
-echo "Download has been finished ..."
-
-# Extracting and after that deleting download archive file
-for JAVA_ARCHIVE in `ls`
-do
-    if [ -f $JAVA_ARCHIVE ]; then 
-        echo "Extrancting of $JAVA_ARCHIVE has been started..."
-        tar -xvf $JAVA_ARCHIVE -C $TEMP_HOME
-        echo "Extrancting has been finised..."
-        echo "Completely removing $JAVA_ARCHIVE ..."
-        rm -f $JAVA_ARCHIVE
-        echo "$JAVA_ARCHIVE has been removed completely..."
-    fi
-done
-
-# Renaming extracted directory name to 'java'
-for JAVA_EXTRACTED_DIRECTORY in `ls`
-do
-    if [ -d $JAVA_EXTRACTED_DIRECTORY ]; then 
-        echo "Renaming $JAVA_EXTRACTED_DIRECTORY to java ..."
-        mv -f $JAVA_EXTRACTED_DIRECTORY java
-        echo "Renamed $JAVA_EXTRACTED_DIRECTORY to java ..."
-    fi
-done
-echo "Moving java to $JAVA_HOME ..."
-
-# Copying 'java' directory to 'java-home'
-cp -rf java $JAVA_HOME
-echo "java has been moden to $JAVA_HOME ..."
-echo "Cleaning $TEMP_HOME ..."
-
-# Cleaning ojul's temporary directory
-for JUNK_FILE in `ls`
-do
-    rm -rf $JUNK_FILE
-done
-echo "Cleaning $TEMP_HOME has been finished..."
 
 # Running ojul by default
-ojul
-
+bash $OJUL_HOME/update.sh
--- update.sh
+++ update.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-clear
+#clear
 
 # Initializing
+USER_HOME="/home/$USER"
 OJUL_HOME="$USER_HOME/.ojul"
 TEMP_HOME="$OJUL_HOME/temp"
 JAVA_HOME="$OJUL_HOME/oracle-java"
 
+if [ ! -e $JAVA_HOME/java/release ]; then
+	
+	bash $OJUL_HOME/install.sh
+fi
+
 # Running ojul-updater
-java -jar $OJUL_HOME/ojul.jar
+#$JAVA_HOME/java/bin/./java -jar $OJUL_HOME/ojul.jar
 
 # Changing directory to ojul's temporary directory
 cd $TEMP_HOME
@@ -15,24 +21,24 @@ cd $TEMP_HOME
 # Extracting and after that deleting downloaded archive files
 for JAVA_ARCHIVE in `ls`
 do
-    if [ -f $JAVA_ARCHIVE ]; then 
-        echo "Extrancting of $JAVA_ARCHIVE has been started..."
-        tar -xvf $JAVA_ARCHIVE -C $TEMP_HOME
-        echo "Extrancting has been finised..."
-        echo "Completely removing $JAVA_ARCHIVE ..."
-        rm -f $JAVA_ARCHIVE
-        echo "$JAVA_ARCHIVE has been removed completely..."
-    fi
+	if [ -f $JAVA_ARCHIVE ]; then 
+	    echo "Extrancting of $JAVA_ARCHIVE has been started..."
+	    tar -xvf $JAVA_ARCHIVE -C $TEMP_HOME
+	    echo "Extrancting has been finised..."
+	    echo "Completely removing $JAVA_ARCHIVE ..."
+	    rm -f $JAVA_ARCHIVE
+	    echo "$JAVA_ARCHIVE has been removed completely..."
+	fi
 done
 
 # Renaming extracted directory name to 'java'
 for JAVA_EXTRACTED_DIRECTORY in `ls`
 do
-    if [ -d $JAVA_EXTRACTED_DIRECTORY ]; then 
-        echo "Renaming $JAVA_EXTRACTED_DIRECTORY to java ..."
-        mv -f $JAVA_EXTRACTED_DIRECTORY java
-        echo "Renamed $JAVA_EXTRACTED_DIRECTORY to java ..."
-    fi
+	if [ -d $JAVA_EXTRACTED_DIRECTORY ]; then 
+	    echo "Renaming $JAVA_EXTRACTED_DIRECTORY to java ..."
+	    mv -f $JAVA_EXTRACTED_DIRECTORY java
+	    echo "Renamed $JAVA_EXTRACTED_DIRECTORY to java ..."
+	fi
 done
 echo "Moving java to $JAVA_HOME ..."
 
@@ -44,7 +50,8 @@ echo "Cleaning $TEMP_HOME ..."
 # Cleaning ojul's temporary directory
 for JUNK_FILE in `ls`
 do
-    rm -rf $JUNK_FILE
+	rm -rf $JUNK_FILE
 done
 echo "Cleaning $TEMP_HOME has been finished..."
+
 
