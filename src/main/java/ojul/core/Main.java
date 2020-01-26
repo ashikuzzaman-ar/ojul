cb67a8689f6d649404a7f46f8bcf23848d6a7a6c
Md. Ashikuzzaman <ashikuzzaman.ar@gmail.com>
2020-01-26 22:04:31 +0600

    Temporary Commit

--- src/main/java/ojul/core/Main.java
+++ src/main/java/ojul/core/Main.java
@@ -1,16 +1,19 @@
 /**
- * OJUL(Oracle Java Updater for Linux) is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    OJUL(Oracle Java Updater for Linux) is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with OJUL(Oracle Java Updater for Linux).  If not, see <http://www.gnu.org/licenses/>.
+ * OJUL(Oracle Java Updater for Linux) is free software: you can redistribute it
+ * and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * OJUL(Oracle Java Updater for Linux) is distributed in the hope that it will
+ * be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with OJUL(Oracle Java Updater for Linux). If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 
 package ojul.core;
@@ -34,19 +37,21 @@ public class Main {
 	public static void main(String[] args) {
 
 		try {
-
+			// Starting from here
 			DownloadableURL downloadableURL = new JDKDownloadableURL();
 			CheckUpdateAvailability checkUpdateAvailability = new CheckUpdateAvailability(downloadableURL);
 			if (checkUpdateAvailability.isUpdateAvailable() > 0) {
 
 				System.out.println("New JAVA update is available!");
-				System.out.println("Installed JAVA version is : " + checkUpdateAvailability.getLocalJavaVersion());
+				System.out.println(
+						"Installed JAVA version is : " + checkUpdateAvailability.getLocalJavaVersion());
 				System.out.println("Latest JAVA version is: " + downloadableURL.getCurrentJDKVersion());
 
 				Downloader downloader = new Downloader(downloadableURL);
+				System.out.println("You will need to download " + downloader.getJavaDetails().getSize()
+						+ " of archive file.");
 				System.out.println(
-						"You will need to download " + downloader.getJavaDetails().getSize() + " of archive file.");
-				System.out.println("Make sure not to terminate the program by force before finished automatically!!!");
+						"Make sure not to terminate the program by force before finished automatically!!!");
 				System.out.print("Do you want to download and update your JAVA version ? (Y/n) : ");
 				scanner = new Scanner(System.in);
 				String token = scanner.next().trim().toLowerCase();
@@ -65,7 +70,8 @@ public class Main {
 			} else {
 
 				System.out.println("JAVA is updated!");
-				System.out.println("Installed JAVA version is : " + checkUpdateAvailability.getLocalJavaVersion());
+				System.out.println(
+						"Installed JAVA version is : " + checkUpdateAvailability.getLocalJavaVersion());
 			}
 		} catch (Exception e) {
 
